a1a9941c5e64b596a30d900cb9843f8fff95c3f6
Andrew McOlash <amcolash@gmail.com>
2022-12-09 23:24:12 -0800

    refactor out header

--- src/Guide.jsx
+++ src/Guide.jsx
@@ -2,16 +2,10 @@ import React, { useCallback, useEffect, useState } from 'react';
 import Highlighter from 'react-highlight-words';
 import { style } from 'typestyle';
 
-import { debounce, lastGuide, lastScroll, lastZoom, mod, SERVER, useDebounce } from './util';
+import { debounce, lastGuide, lastScroll, lastZoom, SERVER, useDebounce } from './util';
 
 import ArrowUp from './icons/arrow-up.svg';
-import ArrowBarUp from './icons/arrow-bar-up.svg';
-import ArrowBarDown from './icons/arrow-bar-down.svg';
-import Dash from './icons/dash-lg.svg';
-import Plus from './icons/plus-lg.svg';
-import X from './icons/x-lg.svg';
-
-const zoomFactor = 0.2;
+import { Header } from './Header';
 
 export function Guide(props) {
   const guide = props.guide;
@@ -90,57 +84,16 @@ export function Guide(props) {
 
   return (
     <div>
-      <header
-        style={{
-          position: 'fixed',
-          top: 0,
-          left: 0,
-          width: 'calc(100% - 1.5rem)',
-          padding: '0.75rem',
-          display: 'flex',
-          justifyContent: 'space-between',
-          background: 'var(--background)',
-          borderBottom: '2px solid #33353d',
-        }}
-      >
-        <button onClick={() => props.setGuide()} style={{ padding: '0 1rem' }}>
-          <X className="icon" />
-        </button>
-
-        <div style={{ display: 'flex', gap: '0.5rem', alignItems: 'center', maxWidth: 'min(60%, 40vw)', width: '100%' }}>
-          <input
-            type="search"
-            value={search}
-            onChange={(e) => setSearch(e.target.value)}
-            placeholder="Search Guide"
-            style={{ flex: 1, minWidth: '7rem' }}
-          />
-
-          {searchLength > 0 && (
-            <>
-              <span style={{ marginLeft: '0.5rem' }}>
-                {searchIndex + 1} / {searchLength}
-              </span>
-              <button onClick={() => setSearchIndex(mod(searchIndex - 1, searchLength))} style={{ padding: '0 1rem' }}>
-                <ArrowBarUp className="icon" />
-              </button>
-              <button onClick={() => setSearchIndex(mod(searchIndex + 1, searchLength))} style={{ padding: '0 1rem' }}>
-                <ArrowBarDown className="icon" />
-              </button>
-            </>
-          )}
-        </div>
-
-        <div style={{ display: 'flex', gap: '0.75rem', alignItems: 'center' }}>
-          {zoom.toFixed(1)}x
-          <button disabled={zoom <= 1} onClick={() => setZoom(zoom - zoomFactor)} style={{ fontWeight: 'bold', padding: '0 1rem' }}>
-            <Dash className="icon" />
-          </button>
-          <button disabled={zoom >= 1.99} onClick={() => setZoom(zoom + zoomFactor)} style={{ fontWeight: 'bold', padding: '0 1rem' }}>
-            <Plus className="icon" />
-          </button>
-        </div>
-      </header>
+      <Header
+        setGuide={props.setGuide}
+        search={search}
+        setSearch={setSearch}
+        searchLength={searchLength}
+        searchIndex={searchIndex}
+        setSearchIndex={setSearchIndex}
+        zoom={zoom}
+        setZoom={setZoom}
+      />
 
       {scrollTop && (
         <button
---
+++ src/Header.jsx
@@ -0,0 +1,74 @@
+import React, { useEffect, useState } from 'react';
+
+import ArrowBarUp from './icons/arrow-bar-up.svg';
+import ArrowBarDown from './icons/arrow-bar-down.svg';
+import Dash from './icons/dash-lg.svg';
+import Plus from './icons/plus-lg.svg';
+import X from './icons/x-lg.svg';
+import { mod } from './util';
+
+const zoomFactor = 0.2;
+
+export function Header(props) {
+  return (
+    <header
+      style={{
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        width: 'calc(100% - 1.5rem)',
+        padding: '0.75rem',
+        display: 'flex',
+        justifyContent: 'space-between',
+        background: 'var(--background)',
+        borderBottom: '2px solid #33353d',
+      }}
+    >
+      <button onClick={() => props.setGuide()} style={{ padding: '0 1rem' }}>
+        <X className="icon" />
+      </button>
+
+      <div style={{ display: 'flex', gap: '0.5rem', alignItems: 'center', maxWidth: 'min(60%, 40vw)', width: '100%' }}>
+        <input
+          type="search"
+          value={props.search}
+          onChange={(e) => props.setSearch(e.target.value)}
+          placeholder="Search Guide"
+          style={{ flex: 1, minWidth: '7rem' }}
+        />
+
+        {props.searchLength > 0 && (
+          <>
+            <span style={{ marginLeft: '0.5rem' }}>
+              {props.searchIndex + 1} / {props.searchLength}
+            </span>
+            <button onClick={() => props.setSearchIndex(mod(props.searchIndex - 1, props.searchLength))} style={{ padding: '0 1rem' }}>
+              <ArrowBarUp className="icon" />
+            </button>
+            <button onClick={() => props.setSearchIndex(mod(props.searchIndex + 1, props.searchLength))} style={{ padding: '0 1rem' }}>
+              <ArrowBarDown className="icon" />
+            </button>
+          </>
+        )}
+      </div>
+
+      <div style={{ display: 'flex', gap: '0.75rem', alignItems: 'center' }}>
+        {props.zoom.toFixed(1)}x
+        <button
+          disabled={props.zoom <= 1}
+          onClick={() => props.setZoom(props.zoom - zoomFactor)}
+          style={{ fontWeight: 'bold', padding: '0 1rem' }}
+        >
+          <Dash className="icon" />
+        </button>
+        <button
+          disabled={props.zoom >= 1.99}
+          onClick={() => props.setZoom(props.zoom + zoomFactor)}
+          style={{ fontWeight: 'bold', padding: '0 1rem' }}
+        >
+          <Plus className="icon" />
+        </button>
+      </div>
+    </header>
+  );
+}
