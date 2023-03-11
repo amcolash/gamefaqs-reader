e17244da694b60ebebea9a2240b905afd98df47d
Andrew McOlash <amcolash@gmail.com>
2023-03-10 17:16:38 -0800

    add electron

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite';
+import electron from 'vite-electron-plugin';
 import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
-  plugins: [svgr({ exportAsDefault: true })],
+  build: {
+    outDir: 'build/dist',
+  },
+  plugins: [
+    svgr({ exportAsDefault: true }),
+    electron({
+      include: ['electron'],
+      outDir: 'build/dist-electron',
+    }),
+  ],
   server: {
     host: '0.0.0.0',
   },
