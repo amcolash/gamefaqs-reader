2e6967a9aa922fbf49c54ebc2777f8a14ac85b11
Andrew McOlash <amcolash@gmail.com>
2023-03-11 11:03:59 -0800

    keep track of window size

--- electron/main.js
+++ electron/main.js
@@ -1,11 +1,43 @@
 import { app, BrowserWindow } from 'electron';
+import windowStateKeeper from 'electron-window-state';
 
 app.whenReady().then(() => {
-  const win = new BrowserWindow();
+  const PROD = app.isPackaged;
 
-  if (process.env.VITE_DEV_SERVER_URL) {
-    win.loadURL(process.env.VITE_DEV_SERVER_URL);
-  } else {
+  // Init window state manager
+  let mainWindowState = windowStateKeeper({
+    defaultWidth: 1000,
+    defaultHeight: 800,
+  });
+
+  let prodOptions = { fullscreen: true };
+  let devOptions = { x: mainWindowState.x, y: mainWindowState.y, width: mainWindowState.width, height: mainWindowState.height };
+
+  // Open window in fullscreen in production mode, open maximized in dev
+  const win = new BrowserWindow(PROD ? prodOptions : devOptions);
+
+  // Keep track of the window state
+  if (!PROD) mainWindowState.manage(win);
+
+  // Bypass CORS, from: https://stackoverflow.com/a/56376799/2303432
+  const filter = {
+    urls: ['https://gamefaqs.gamespot.com/*'],
+  };
+
+  win.webContents.session.webRequest.onBeforeSendHeaders(filter, (details, callback) => {
+    details.requestHeaders['Origin'] = null;
+    details.headers['Origin'] = null;
+    callback({ requestHeaders: details.requestHeaders });
+  });
+
+  // Load index page
+  if (PROD) {
     win.loadFile('build/dist/index.html');
+  } else {
+    win.loadURL(process.env.VITE_DEV_SERVER_URL);
   }
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
 });
