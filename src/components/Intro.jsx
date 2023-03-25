10d1e9a9c7c847c90dcb8afc0f781ac5336053cb
Andrew McOlash <amcolash@gmail.com>
2023-03-24 22:47:31 -0700

    add gamepad controls image to intro

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -23,48 +23,28 @@ export function Intro(props) {
           display: 'flex',
           flexDirection: 'column',
           alignItems: 'center',
-          textAlign: 'center',
-          maxWidth: 'var(--maxWidth)',
+          textAlign: 'justify',
+          maxWidth: '1100px',
           overflow: 'auto',
           padding: '0.5rem',
         }}
       >
-        <h1>Welcome to GameFAQs Reader!</h1>
-        <h3>
-          This application lets you read game guides on the go. It even automatically saves them for offline use. To get started, just
-          search for a game and select a guide.
-        </h3>
+        <div style={{ maxWidth: '485px', marginBottom: '3rem' }}>
+          <h1>Welcome to GameFAQs Reader!</h1>
+          <h3>
+            This application lets you read game guides and automatically saves them for offline use on the go. To get started, just search
+            for a game and select a guide.
+          </h3>
 
-        <h3>
-          This application supports controller input out of the box. I would recommend using the default steam controller template
-          ("Templates -{'>'} Gamepad") for this application.
-        </h3>
+          <h3>
+            This application supports controller input out of the box. I recommend using the default steam controller template ("Templates -
+            {'>'} Gamepad") for the default button mapping.
+          </h3>
+        </div>
 
-        <h4>General</h4>
-        <ul style={{ textAlign: 'left' }}>
-          <li>Cross (Playstation) / A (Xbox) / B (Nintendo) - Confirm</li>
-          <li>Circle (Playstation) / B (Xbox) / A (Nintendo) - Back / Exit</li>
+        <img src="./gamepad.png" style={{ maxWidth: '100%' }} />
 
-          <li>D-Pad / Left Stick - Navigate Items</li>
-          <li>Right Stick - Scroll</li>
-
-          <li>Left Trigger - Page Up</li>
-          <li>Right Trigger - Page Down</li>
-        </ul>
-
-        <h4>Onscreen Keyboard</h4>
-        <ul style={{ textAlign: 'left' }}>
-          <li>Cross (Playstation) / A (Xbox) / B (Nintendo) - Select Letter / Open Keyboard</li>
-          <li>Square (Playstation) / X (Xbox) / Y (Nintendo) - Backspace</li>
-          <li>Circle (Playstation) / B (Xbox) / A (Nintendo) - Close Keyboard / Clear Text</li>
-
-          <li>D-Pad / Left Stick - Navigate Keys</li>
-
-          <li>Left Bumper - Move Cursor Back</li>
-          <li>Right Bumper - Move Cursor Forwards</li>
-        </ul>
-
-        <button style={{ marginTop: '2rem' }} onClick={() => props.setShowIntro(false)}>
+        <button style={{ marginTop: '3rem' }} onClick={() => props.setShowIntro(false)}>
           Get Started
         </button>
       </div>
